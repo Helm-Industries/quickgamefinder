5988a64404a7ac9c1e53a9d65fc4dbc4b511a78e
nextrie <40547005+nextrie@users.noreply.github.com>
2019-09-20 00:17:28 +0200

    Systeme changement de page + debut inscription

---
+++ qgf_client/js_tools/changePage.js
@@ -0,0 +1,17 @@
+//SCRIPT POUR LE CHANGEMENT D'AFFICHAGE DYNAMIQUE DE LA PAGE
+var registerButton = document.getElementById("registerPageButton");
+var currentPage = "login";
+$("#bodyContent").load("./html/index/" + currentPage + ".html");
+$("#" + currentPage).attr("href", "./css/index/" + currentPage + ".css");
+
+function changeCurrentPage(nextPage) 
+{
+    $("#bodyContent").fadeOut(200, () => {
+        $("#bodyContent").empty();
+        $("#bodyContent").load("./html/index/" + nextPage + ".html");
+        $("#" + currentPage).attr("href", "./css/index/" + nextPage + ".css");
+        currentPage = nextPage;
+    });
+    $("#bodyContent").fadeIn();
+
+}
